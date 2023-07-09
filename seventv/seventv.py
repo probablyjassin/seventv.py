0bae1ba3c1f7ed5fffc46efa00ea40eb953ef71d
probablyjassin <aouanijassin@gmail.com>
2023-07-09 23:10:16 +0200

    v2.3.2 different error handling by adding own exception class

--- seventv/seventv.py
+++ seventv/seventv.py
@@ -96,6 +96,9 @@ class seventv:
         async with self.session.post(url, json=payload, headers=headers) as response:
             response_data = await response.json()
             if response_data.get('errors', {}):
-                raise Exception(response_data.get('errors', {})[0].get('message', {}))
+                raise seventvException(response_data.get('errors', {})[0].get('message', {}))
             emote_objects = create_emote_objects(response_data)
             return emote_objects
+
+class seventvException(Exception):
+        pass
