e3b3199a34657cc255d57236c6996bc228fede84
probablyjassin <aouanijassin@gmail.com>
2023-07-09 23:58:33 +0200

    v2.23 see prev. commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='seventv',
-    version="2.3.2",
+    version="2.3.3",
     author='Jässin Aouani',
     author_email='jassin@aouani.de',
     description='An asynchronous API-wrapper for 7tv.app',
